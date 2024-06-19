0e6692aab9998d65981cfe02d0a87b170453c21f
Muzhe Wu <635717823@qq.com>
2024-06-19 00:23:10 -0400

    proof of concept with xampp and fpdf

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <meta charset="UTF-8">
-<title>Simple Example of PDF file using PHP and MySQL</title>
+<title>Fake certificate</title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 <div class="container" style="padding-top:50px">
-    <h2>Generate PDF file from MySQL Using PHP</h2>
-    <form class="form-inline" method="post" action="generate_pdf.php">
+    <h2>Congratualtions, Henry! You just complete the course! Here is your certificate.</h2>
+    <form class="form-inline" method="post" action="verify.php">
         <button type="submit" id="pdf" name="generate_pdf" class="btn btn-primary"><i class="fa fa-pdf" "="" aria-hidden=" true"></i>
-            Generate PDF</button>
+            Get Certificate</button>
     </form>
 </div>
